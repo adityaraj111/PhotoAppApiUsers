c53a7393bbcdee41e548f77027d982ecf7a6a0b8
Aditya Raj <adityaraj@Adityas-MacBook-Pro.local>
2022-09-23 16:41:17 +0530

    First Commit

---
+++ src/test/java/com/adityacode/photoapp/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityacode.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
