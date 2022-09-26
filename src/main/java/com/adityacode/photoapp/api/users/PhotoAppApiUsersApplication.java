3e1dc1ee04162dfc6804a95a906567eeb22ba811
adityaraj <adityaraj@localhost>
2022-09-26 16:20:34 +0530

    Eureka Client (Users MS)

--- src/main/java/com/adityacode/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/com/adityacode/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -2,7 +2,9 @@ package com.adityacode.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PhotoAppApiUsersApplication {
 
