55583c87c28e14bf5db5a08dec0d46440205cd81
adityaraj <adityaraj@localhost>
2022-10-02 09:49:33 +0530

    Added Validation to Request Model class

--- src/main/java/com/adityacode/photoapp/api/users/ui/model/CreateUserRequestModel.java
+++ src/main/java/com/adityacode/photoapp/api/users/ui/model/CreateUserRequestModel.java
@@ -1,10 +1,25 @@
 package com.adityacode.photoapp.api.users.ui.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CreateUserRequestModel {
 	
+	@NotNull(message = "First name cannot be null")
+	@Size(min = 2, message = "First name must not be less than 2 characters")
 	private String firstName;
+	
+	@NotNull(message = "Last name cannot be null")
+	@Size(min = 2, message = "Last name must not be less than 2 characters")
 	private String lastName;
+	
+	@NotNull(message = "Password cannot be null")
+	@Size(min = 8, max = 16, message = "Password must be equal or greater than 8 characters and less than 16 characters")
 	private String password;
+	
+	@NotNull(message = "Email cannot be null")
+	@Email
 	private String email;
 	
 	public String getFirstName() {
