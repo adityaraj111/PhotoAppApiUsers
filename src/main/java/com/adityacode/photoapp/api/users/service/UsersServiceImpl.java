2d4e2e0c11f4f11d4af9910f8b53f26265bb13ed
adityaraj <adityaraj@localhost>
2022-10-02 21:54:34 +0530

    Added Password Encoder code

--- src/main/java/com/adityacode/photoapp/api/users/service/UsersServiceImpl.java
+++ src/main/java/com/adityacode/photoapp/api/users/service/UsersServiceImpl.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.adityacode.photoapp.api.users.data.UserEntity;
@@ -17,10 +18,13 @@ public class UsersServiceImpl implements UsersService {
 	
 	UsersRepository userRepository;
 	
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Autowired
-	public UsersServiceImpl(UsersRepository userRepository) {
+	public UsersServiceImpl(UsersRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
 		super();
 		this.userRepository = userRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@Override
@@ -28,10 +32,12 @@ public class UsersServiceImpl implements UsersService {
 		// TODO Auto-generated method stub
 		
 		userDetails.setUserId(UUID.randomUUID().toString());  // random alpha-numeric string of chars that can be user as a ID
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
+		
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPassword("test");
+		//userEntity.setEncryptedPassword("test");
 		
 		userRepository.save(userEntity);
 		
