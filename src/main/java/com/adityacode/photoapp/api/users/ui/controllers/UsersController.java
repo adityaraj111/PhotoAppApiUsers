46afdce9783beed292984f8cc22aa208b704f0c8
adityaraj <adityaraj@localhost>
2022-10-02 09:50:20 +0530

    added valid annotation for createUser Handler

--- src/main/java/com/adityacode/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/adityacode/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,9 +1,12 @@
 package com.adityacode.photoapp.api.users.ui.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +25,7 @@ public class UsersController {
 	}
 	
 	@PostMapping
-	public String createUser(CreateUserRequestModel userDetails) {
+	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
 		
 		return "Create User is called";
 	}
