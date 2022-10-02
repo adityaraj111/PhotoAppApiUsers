2c445b14022516203dbd9eb441d054cec503bb82
adityaraj <adityaraj@localhost>
2022-10-02 09:51:12 +0530

    Added UserDto and Service Layer class and interface

---
+++ src/main/java/com/adityacode/photoapp/api/users/service/UsersService.java
@@ -0,0 +1,9 @@
+package com.adityacode.photoapp.api.users.service;
+
+import com.adityacode.photoapp.api.users.shared.UserDto;
+
+public interface UsersService {
+
+	UserDto createUser(UserDto userDetails);
+
+}
---
+++ src/main/java/com/adityacode/photoapp/api/users/service/UsersServiceImpl.java
@@ -0,0 +1,13 @@
+package com.adityacode.photoapp.api.users.service;
+
+import com.adityacode.photoapp.api.users.shared.UserDto;
+
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
