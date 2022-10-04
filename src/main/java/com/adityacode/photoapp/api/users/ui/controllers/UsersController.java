e7fbca635a99f31eefa7b2273cba9cf937dabf4d
adityaraj <adityaraj@localhost>
2022-10-04 17:29:47 +0530

    Added handler config to accept and return both json/xml

--- src/main/java/com/adityacode/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/adityacode/photoapp/api/users/ui/controllers/UsersController.java
@@ -7,6 +7,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,26 +26,29 @@ public class UsersController {
 
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	UsersService userService;
-	
+
 	@GetMapping("/status/check")
 	public String status() {
-		return "Working on port "+env.getProperty("local.server.port");
+		return "Working on port " + env.getProperty("local.server.port");
 	}
-	
-	@PostMapping
+
+	@PostMapping(
+			consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+			produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+			)
 	public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
-		
+
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-		
+
 		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
 		UserDto createdUser = userService.createUser(userDto);
-		
+
 		CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
 	}
 }
