16a011eb9a5c85d6a780e322e2568a132bdbae11
adityaraj <adityaraj@localhost>
2022-10-05 10:19:24 +0530

    Implementing Authentication Filter

--- src/main/java/com/adityacode/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/com/adityacode/photoapp/api/users/security/WebSecurity.java
@@ -1,5 +1,7 @@
 package com.adityacode.photoapp.api.users.security;
 
+import javax.servlet.Filter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -22,8 +24,16 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+		.and()
+		.addFilter(getAuthenticationFilter());
 		http.headers().frameOptions().disable();
 
 	}
+
+	private AuthenticationFilter getAuthenticationFilter() throws Exception {
+		AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+		authenticationFilter.setAuthenticationManager(authenticationManager());
+		return authenticationFilter;
+	 }
 }
